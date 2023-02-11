522317c16699df193e94a410599e710ef65f6618
joki004 <jorammumb15.jm@gmail.com>
2023-02-11 14:22:58 +0100

    Body

--- JS/script.js
+++ JS/script.js
@@ -11,5 +11,101 @@ document.querySelector('#close-navbar').onclick = () =>{
 
   window.onscroll = () =>{
     navbar.classList.remove('active');
-    searchForm.classList.remove('active');
   };
+
+  let slides = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next(){
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function prev(){
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+};
+
+function cosmectic_price(){
+    let price = document.getElementsByClassName("cosmetics");
+    Array.from(price).forEach((x) => {
+        if (x.style.display === "none") {
+          x.style.display = "flex";
+        } else {
+          x.style.display = "none";
+        }
+      })
+    
+      let arrow_up = document.getElementsByClassName("arrow-up");
+      Array.from(arrow_up).forEach((x) => {
+        console.log(x);
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+          console.log(x);
+        })
+
+ let arrow_down = document.getElementsByClassName("arrow-down");
+
+   
+       
+}
+
+
+function makeup_price(){
+    let price = document.getElementsByClassName("makeup");
+    Array.from(price).forEach((x) => {
+        if (x.style.display === "none") {
+          x.style.display = "flex";
+        } else {
+          x.style.display = "none";
+        }
+      })
+    
+      let arrow_up = document.getElementsByClassName("arrow-up-make-up");
+      Array.from(arrow_up).forEach((x) => {
+        console.log(x);
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+          console.log(x);
+        })
+
+ let arrow_down = document.getElementsByClassName("arrow-down-make-up");
+
+   
+       
+}
+
+function powder_price(){
+    let price = document.getElementsByClassName("powder");
+    Array.from(price).forEach((x) => {
+        if (x.style.display === "none") {
+          x.style.display = "flex";
+        } else {
+          x.style.display = "none";
+        }
+      })
+    
+      let arrow_up = document.getElementsByClassName("arrow-up-powder");
+      Array.from(arrow_up).forEach((x) => {
+        console.log(x);
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+          console.log(x);
+        })
+
+ let arrow_down = document.getElementsByClassName("arrow-down-powder");
+
+   
+       
+}
